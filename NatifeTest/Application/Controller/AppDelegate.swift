2cf8e29eb083632cae44b1bcfe46b6435575f498
Danylo Klymov <klymov@MacBook-Air-Danylo.local>
2022-01-30 23:14:21 +0200

    Added sotring

--- NatifeTest/Application/Controller/AppDelegate.swift
+++ NatifeTest/Application/Controller/AppDelegate.swift
@@ -22,5 +22,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         
     }
+    
+    var orientationLock = UIInterfaceOrientationMask.portrait
+        
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return self.orientationLock
+    }
 }
 
